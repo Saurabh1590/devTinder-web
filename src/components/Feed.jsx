bca7ad4ef1e87c350b33208132f1af42bf08008a
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-06-03 02:44:57 +0530

    redirect to login if not authunticated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Feed = () => {
   console.log("Feed component rendered");
-  return <div className="text-2xl text-center mt-10">Feed</div>;
+  return <div >Feed</div>;
 };
 
 export default Feed;
